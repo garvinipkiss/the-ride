9a79a58f1b350e1d167192b47b6674deb844265d
garvinipkiss <garvinipkiss@gmail.com>
2018-05-20 22:30:41 +0300

    updated installed apps in settings

--- ride/ride/settings.py
+++ ride/ride/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drivers.apps.DriversConfig',
+    'rider',
+    'star_ratings',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
